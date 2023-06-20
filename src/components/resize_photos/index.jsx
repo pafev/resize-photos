03b3ce58fab48883ffcea495b6a3bd4b6de98013
pafev <97859269+pafev@users.noreply.github.com>
2023-06-20 17:34:16 -0300

    Update index.jsx

--- src/components/resize_photos/index.jsx
+++ src/components/resize_photos/index.jsx
@@ -2,32 +2,25 @@ import { useState } from "react";
 import resize from "./resize";
 
 export function ResizeImages() {
-  const [imageToShow, setImageToShow] = useState();
 
   function callback(image) {
     console.log(image);
-
-    setImageToShow(image.name);
   }
 
   function handleChange(ev) {
     const files = ev.target.files;
 
-    if (files.length === 0) {
-      return;
+    if (files.length > 0) {
+      console.log(files[0]);
+  
+      resize(files[0], callback);
     }
-
-    console.log(files[0]);
-
-    resize(files[0], callback);
   }
 
   return (
     <div>
       <label htmlFor="addImage">Coloque uma foto para ser comprimida</label>
       <input type="file" onChange={handleChange} id="addImage" />
-
-      <img src={imageToShow} alt="imagem selecionada e comprimida" />
     </div>
   );
 }
